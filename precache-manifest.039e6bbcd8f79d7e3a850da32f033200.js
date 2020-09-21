19fa76224e6beffd350a976f6f447357e7771dbe
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-09-21 18:44:10 +0100

    Updates

---
+++ precache-manifest.039e6bbcd8f79d7e3a850da32f033200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff35636ed168ec322bdfb8538f293a7",
+    "url": "/ygopro/index.html"
+  },
+  {
+    "revision": "7de977eca9c45cbf789c",
+    "url": "/ygopro/static/css/main.0d725fc7.chunk.css"
+  },
+  {
+    "revision": "38dd2a2c589bdb2fa38f",
+    "url": "/ygopro/static/js/2.d994ab83.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/ygopro/static/js/2.d994ab83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de977eca9c45cbf789c",
+    "url": "/ygopro/static/js/main.8d9a7559.chunk.js"
+  },
+  {
+    "revision": "fb5594bb8d526c589cfe",
+    "url": "/ygopro/static/js/runtime-main.766ed7fb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ygopro/precache-manifest.039e6bbcd8f79d7e3a850da32f033200.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ygopro/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
